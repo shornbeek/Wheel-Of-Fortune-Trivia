9576d45153990f7d1db8d6adb1b4dcd903769949
Stephen Hornbeek <hornbeeks@gmail.com>
2018-12-01 19:55:01 -0800

    questions array added

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+console.log("Hi Pat and Vanna");
+
+var questions = [
+    
+    {
+        question: "When was the first Air Max made?",
+        answer: ["2000", "1987", "1995", "1988"],
+        name: "firstAirmax",
+        correct: "1987",
+        divClass: ".firstAirmax"
+    },
+
+    {
+        question: "When was the first Air Max made?",
+        answer: ["2000", "1987", "1995", "1988"],
+        name: "firstAirmax",
+        correct: "1987",
+        divClass: ".firstAirmax"
+    },
+
+    {
+        question: "When was the first Air Max made?",
+        answer: ["2000", "1987", "1995", "1988"],
+        name: "firstAirmax",
+        correct: "1987",
+        divClass: ".firstAirmax"
+    },
+
+    {
+        question: "When was the first Air Max made?",
+        answer: ["2000", "1987", "1995", "1988"],
+        name: "firstAirmax",
+        correct: "1987",
+        divClass: ".firstAirmax"
+    }   
+];
+
+console.log(questions);
